c86b33fca2341eb9138c8c64192eedd5a0e479b1
Maxim Dvorovoi <dvorovoi@semalt.com>
2018-11-01 18:57:33 +0200

    dev 0.1

---
+++ views/site/index.php
@@ -0,0 +1,152 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'My Yii Application';
+?>
+
+<div class="site-index">
+	<div id="slider">
+		<div class="head-text">
+			<h1>Оформление букетов от Flower Bar</h1>
+			<h3 >Мы создадим для вас самые стильные и запоминающиеся проекты</h3>
+
+			<a href="/YII2/flower-site/web/about" class="head-button">Узнать больше</a><br><br>
+		</div>
+
+		<div class="text1">
+			<div class="container">
+				<h2>Команда FLOWERBAR создает стильные, запоминающиеся
+					проекты, в которых масштаб гармонично сочетается
+					с вниманием к деталям</h2>
+				<hr>
+				<br><br>
+
+				<div class="products-list">
+					<a href="#" class="path">/ букеты</a>
+					<a href="#" class="tag">популярные</a>
+					<div class="products">
+						<a href="/product/4">
+							<div class="crop_image">
+								<img src="http://rosenville.com.ua/image/cache/catalog/ruchnaja_rabota/mjagkie_igrushki/342-700x700.jpg" class="image" style="width: 300px">
+							</div>
+						</a>
+						<div class="title">
+							Букет "Беатрис"
+						</div>
+
+						<div class="views">
+							<div>800 грн</div>
+							<a href="#0" class="cd-add-to-cart">+<i class="fas fa-shopping-bag"></i></a>
+						</div>
+					</div>
+					<div class="products">
+						<a href="/product/4">
+							<div class="crop_image">
+								<img src="http://rosenville.com.ua/image/cache/catalog/ruchnaja_rabota/mjagkie_igrushki/343-700x700.jpg" class="image" style="width: 300px">
+							</div>
+						</a>
+						<div class="title">
+							Букет "Беатрис"
+						</div>
+
+						<div class="views">
+							<div>800 грн</div>
+							<a href="#0" class="cd-add-to-cart">+<i class="fas fa-shopping-bag"></i></a>
+						</div>
+					</div>
+					<div class="products">
+						<a href="/product/4">
+							<div class="crop_image">
+								<img src="http://rosenville.com.ua/image/cache/catalog/ruchnaja_rabota/mjagkie_igrushki/356-700x700.jpg" class="image" style="width: 300px">
+							</div>
+						</a>
+						<div class="title">
+							Букет "Беатрис"
+						</div>
+
+						<div class="views">
+							<div>800 грн</div>
+							<a href="#0" class="cd-add-to-cart">+<i class="fas fa-shopping-bag"></i></a>
+						</div>
+					</div>
+					<div class="products">
+						<a href="/product/4">
+							<div class="crop_image">
+								<img src="http://rosenville.com.ua/image/cache/catalog/ruchnaja_rabota/mjagkie_igrushki/352-700x700.jpg" class="image" style="width: 300px">
+							</div>
+						</a>
+						<div class="title">
+							Букет "Беатрис"
+						</div>
+
+						<div class="views">
+							<div>800 грн</div>
+							<a href="#0" class="cd-add-to-cart">+<i class="fas fa-shopping-bag"></i></a>
+						</div>
+					</div>
+					<div class="products">
+						<a href="/product/4">
+							<div class="crop_image">
+								<img src="http://rosenville.com.ua/image/cache/catalog/ruchnaja_rabota/mjagkie_igrushki/355-700x700.jpg" class="image" style="width: 300px">
+							</div>
+						</a>
+						<div class="title">
+							Букет "Беатрис"
+						</div>
+
+						<div class="views">
+							<div>800 грн</div>
+							<a href="#0" class="cd-add-to-cart">+<i class="fas fa-shopping-bag"></i></a>
+						</div>
+					</div>
+					<div class="products">
+						<a href="/product/4">
+							<div class="crop_image">
+								<img src="http://rosenville.com.ua/image/cache/catalog/ruchnaja_rabota/mjagkie_igrushki/347-700x700.jpg" class="image" style="width: 300px">
+							</div>
+						</a>
+						<div class="title">
+							Букет "Беатрис"
+						</div>
+
+						<div class="views">
+							<div>800 грн</div>
+							<a href="#0" class="cd-add-to-cart">+<i class="fas fa-shopping-bag"></i></a>
+						</div>
+					</div>
+				</div>
+
+				<br><br>
+				<a href="/YII2/flower-site/web/" class="head-button">Показать все</a>
+				<br><br><br><br>
+			</div>
+			<div class="down">🢓</div>
+		</div>
+		<br><br><br><br>
+		<div class="text1">
+			<div class="container">
+				<div class="products-list">
+					<div class="baner">
+						<i class="fas fa-atom"></i>
+						<h3>Дизайнерские букеты</h3>
+						<h4>Букет для любимой на годовщину, букет для младшей сестры, букет начальнику — мы сделаем так чтобы получатель вашего букета был в восторге!</h4>
+					</div>
+					<div class="baner">
+						<i class="fas fa-stopwatch"></i>
+						<h3>Скорость</h3>
+						<h4>Букет нужен срочно? Без проблем! Всегда в наличии готовые свежие букеты!</h4>
+					</div>
+					<div class="baner">
+						<i class="fas fa-lightbulb"></i>
+						<h3>Понимание</h3>
+						<h4>Знаете, что хотите но не можете выразить словами? — Не переживайте, наши флористы поймут каждого!</h4>
+					</div>
+				</div>
+			</div>
+		</div>
+
+	</div>
+
+
+</div>
